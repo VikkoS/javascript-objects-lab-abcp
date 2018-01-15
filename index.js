1bccda65882876119db72716258e7387dc5ece9a
Vika Suhorukova <viktorya.sukhorukova@gmail.com>
2018-01-15 04:19:17 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 var recipes = new Object();
 
-function updateObjectWithKeyAndValue(obj, key, value) {
-  obj[ key ] = value;
-  return obj;
-}
 
 function updateObjectWithKeyAndValue(obj, key, value) {
   var newObj = Object.assign({},{[key]:value});
